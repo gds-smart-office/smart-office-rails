62d0373a00ff8ba024c644e9875e39469a15efdb
Poh Kah Kong <kahkong87mail@gmail.com>
2015-11-26 13:38:07 +0800

    Refactor telegram bot

--- config/application.rb
+++ config/application.rb
@@ -26,52 +26,72 @@ module SmartOffice
     config.active_record.raise_in_transactional_callbacks = true
 
     config.after_initialize do
-      @@thread ||= Thread.new do
-        start_telegram_bot
-      end
+      @@telegram_bot_token =        ENV["telegram_bot_token"]
+      @@telegram_authorized_chats = ENV["telegram_authorized_chats"].split(",").map(&:to_i)
+      @@web_cam_ip =                ENV["web_cam_ip"]
+      @@restart_cooldown =          ENV["restart_cooldown"].to_f
+      
+      run_telegram_bot
     end
     
-    def start_telegram_bot
-      telegram_bot_token = ENV["telegram_bot_token"]
-      telegram_authorized_chats = ENV["telegram_authorized_chats"].split(",").map(&:to_i)      
-      web_cam_ip = ENV["web_cam_ip"]
-      
-      puts "telegram_bot: started"
-      Telegram::Bot::Client.run(telegram_bot_token) do |bot|
-        bot.listen do |message|
-          Message.create(user: message.from.first_name, action: message.text)
-          case message.text
-            when '/pong'
-              puts "telegram_bot: photo request from #{message.from.first_name} #{message.from.last_name}"
-              if telegram_authorized_chats.include?(message.chat.id)
-                puts "telegram_bot: authorized"
-                begin
-                  open('photo.jpg', 'wb') do |file|
-                    file << open("http://#{web_cam_ip}/photo.jpg").read
-                  end
-                  bot.api.send_photo(chat_id: message.chat.id, photo: File.new("photo.jpg"))
-                rescue Exception => e
-                  bot.api.send_message(chat_id: message.chat.id, text: e.message)
-                end
-              else
-                puts "telegram_bot: unauthorized"
-                bot.api.send_photo(chat_id: message.chat.id, photo: File.new("forbidden.jpg"))
+    def run_telegram_bot
+      @@thread ||= Thread.new do      
+        while true do
+          startTime = Time.now
+          begin
+            puts "telegram_bot: started"
+            Telegram::Bot::Client.run(@@telegram_bot_token) do |bot|
+              bot.listen do |message|
+                messageTime = Time.now
+                if (messageTime - startTime) > @@restart_cooldown
+                  perform_telegram_action(bot, message)
+                else
+                  puts "telegram_bot: start: #{startTime}"
+                  puts "telegram_bot: now: #{messageTime}"
+                  puts "telegram_bot: elasped #{messageTime - startTime}"
+                  puts "telegram_bot: cooldown"
+                end                
               end
-            when '/debug'
-              bot.api.send_message(chat_id: message.chat.id, text: "debug: #{message.from.first_name} from chat=#{message.chat.id}")
-            else
-              puts "telegram_bot: else"
+            end
+          rescue Exception => e
+            p e.message
           end
         end
       end
     end
-
+    
+    def perform_telegram_action(bot, message)
+      Message.create(user: message.from.first_name, action: message.text)
+      puts "telegram_bot: #{message.text} by #{message.from.first_name} from chat=#{message.chat.id}"      
+      case message.text
+        when '/pong'
+          puts "telegram_bot: photo request from #{message.from.first_name} #{message.from.last_name}"
+          if @@telegram_authorized_chats.include?(message.chat.id)
+            puts "telegram_bot: authorized"
+            begin
+              open('photo.jpg', 'wb') do |file|
+                file << open("http://#{@@web_cam_ip}/photo.jpg").read
+              end
+              bot.api.send_photo(chat_id: message.chat.id, photo: File.new("photo.jpg"))
+            rescue Exception => e
+              bot.api.send_message(chat_id: message.chat.id, text: e.message)
+            end
+          else
+            puts "telegram_bot: unauthorized"
+            bot.api.send_photo(chat_id: message.chat.id, photo: File.new("forbidden.jpg"))
+          end
+        when '/debug'
+          bot.api.send_message(chat_id: message.chat.id, text: "debug: #{message.from.first_name} from chat=#{message.chat.id}")
+        else
+          puts "telegram_bot: else #{message.text}"
+      end
+    end
+    
     def overlayImage(filename)
       dst = Magick::Image.read("photo.jpg") {self.size = "640x480"}.first
       src = Magick::Image.read(filename).first
       result = dst.composite(src, Magick::CenterGravity, Magick::OverCompositeOp)
       result.write('photo.jpg')
-    end    
-
+    end
   end
 end
