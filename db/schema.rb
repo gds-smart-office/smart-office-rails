d4f6989ae45d34d1d8ab2f17be01a00bf4fe73bd
Kai Kok, Chew <kaikok@gmail.com>
2015-12-11 19:28:06 +0800

    Update db:schema

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20151211080208) do
     t.boolean  "admin"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
